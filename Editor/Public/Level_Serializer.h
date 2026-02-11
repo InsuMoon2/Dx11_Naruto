b1343193f3f5db93e21166ce0aa80d4e648abd3e
InsuMoon2 <soo93689314@gmail.com>
2026-02-11 16:48:05 +0900

    Add Level_Serializer to Editor project
    - Copied Level_Serializer.h to Editor/Public
    - Copied Level_Serializer.cpp to Editor/Private
    - Updated Editor.vcxproj to include Level_Serializer files

---
+++ Editor/Public/Level_Serializer.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+NS_BEGIN(Engine)
+class GameObject;
+NS_END
+
+NS_BEGIN(Editor)
+
+class Level_Serializer
+{
+public:
+    explicit Level_Serializer() = default;
+    ~Level_Serializer() = default;
+
+public:
+    static void Save_Level(const wstring& fileName, const vector<shared_ptr<GameObject>>& gameObjects);
+    static vector<shared_ptr<GameObject>> Load_Level(const wstring& fileName);
+
+    static vector<wstring> Get_SaveFiles();
+
+    static wstring Get_FullPath(const wstring& fileName);
+
+private:
+    static json GameObjectToJson(shared_ptr<GameObject> obj);
+    static shared_ptr<GameObject> JsonToGameObject(const json& j);
+
+private:
+    static constexpr const wchar_t* LEVEL_DIRECTORY = L"../../Client/Bin/Resources/Data/json/Level/";
+
+};
+
+NS_END
