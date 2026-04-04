8ac33fb1d5f21f78c51099c9f4ee8aeb8dd73e9e
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2026-04-04 21:43:42 +0900

    chore : temp

---
+++ Client/Private/ANS_Move.cpp
@@ -0,0 +1,47 @@
+﻿#include "pch.h"
+#include "ANS_Move.h"
+#include "GameObject_Factory.h"
+#include "AnimNotify_Factory.h"
+
+REGISTER_ANIM_NOTIFY_STATE(ANS_Move)
+IMPLEMENT_REFLECTION(ANS_Move)
+
+bool ANS_Move::Register_Properties()
+{
+    auto& info = GetStaticReflectionInfo();
+    info.className = "ANS_Move";
+    info.properties.clear();
+
+    PROPERTY_FLOAT("Speed", _speed, 0.f, 50.f);
+    PROPERTY_BOOL("Use Forward Dir", _useForwardDir);
+    PROPERTY_BOOL("Use Target Dir", _useTargetDir);
+
+    return true;
+}
+
+string ANS_Move::Get_TypeName() const
+{
+    return "ANS_Move";
+}
+
+void ANS_Move::On_Begin(const FAnimNotifyContext& context)
+{
+}
+
+void ANS_Move::On_Tick(const FAnimNotifyContext& context)
+{
+}
+
+void ANS_Move::On_End(const FAnimNotifyContext& context)
+{
+}
+
+json ANS_Move::Serialize_Payload() const
+{
+    return AnimNotifyState::Serialize_Payload();
+}
+
+void ANS_Move::Deserialize_Payload(const json& payload)
+{
+    AnimNotifyState::Deserialize_Payload(payload);
+}
---
+++ Client/Public/ANS_Move.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "AnimNotifyState.h"
+
+NS_BEGIN(Client)
+
+class ANS_Move : public AnimNotifyState
+{
+    GENERATED_BODY(ANS_Move)
+
+public:
+    string Get_TypeName() const override;
+    
+    void On_Begin(const FAnimNotifyContext& context) override;
+    void On_Tick(const FAnimNotifyContext& context) override;
+    void On_End(const FAnimNotifyContext& context) override;
+
+    json Serialize_Payload() const override;
+    void Deserialize_Payload(const json& payload) override;
+
+private:
+    float   _speed = 5.f;
+
+    // 캐릭터 정면방향으로
+    bool    _useForwardDir = true;
+
+    // 타겟팅 방향으로
+    bool    _useTargetDir = false;
+
+    Vec3    _localDirection = Vec3::Zero;
+};
+
+NS_END
