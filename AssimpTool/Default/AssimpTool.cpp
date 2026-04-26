a62554e02c229d271bc5857eee3d299405025384
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2026-04-26 13:12:12 +0900

    feat : 모델

--- AssimpTool/Default/AssimpTool.cpp
+++ AssimpTool/Default/AssimpTool.cpp
@@ -55,7 +55,7 @@ static fs::path ResolveDefaultMeshDir(const wchar_t* executablePath)
         //LR"(Client\Bin\Resources\StaticMesh\KonohaVillage03\Meshes)";
         //LR"(Client\Bin\Resources\Models\LeafNinja)";
         //LR"(Client\Bin\Resources\Models\WhiteZetsu)";
-        LR"(Client\Bin\Resources\Models\LeafNinja)";
+        LR"(Client\Bin\Resources\Models\BoxingGloves)";
         //LR"(Client\Bin\Resources\Models\ShinsuSenju)";
         //LR"(Client\Bin\Resources\Skills\ETC\Meshes\Temp)";
         //LR"(Client\Bin\Resources\Models\SkySphere)";
